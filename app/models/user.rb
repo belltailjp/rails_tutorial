3998fe17fe59865feed982a1e21b6428f2de38fb
Daichi SUZUO <belltailjp@gmail.com>
2014-07-10 02:56:40 +0000

    Change data model to prevent duplication on DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
     validates :name,  presence: true, length: { maximum: 50 }
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                 uniqueness: { case_sensitive: false }
+
+    before_save { self.email = email.downcase }
 end
